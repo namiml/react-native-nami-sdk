2c5c23fb84508abcc906b20e964ff51ecdc77bf0
Kendall Helmstetter Gelner <Kendall.Gelner@kigisoftware.com>
2020-01-28 13:13:05 -0700

    Added "buyProduct" method to bridging.

--- ios/NamiStoreKitHelperBridge.m
+++ ios/NamiStoreKitHelperBridge.m
@@ -57,6 +57,24 @@ RCT_EXPORT_METHOD(anyProductPurchased:(nonnull NSArray*)productIDs completion:(R
     completion(@[[NSNumber numberWithBool:active]]);
 }
 
+RCT_EXPORT_METHOD(buyProduct:(nonnull NSString*)productID completion:(RCTResponseSenderBlock)completion)
+{
+    [[NamiStoreKitHelper shared] productsForProductIdentifersWithProductIDs:@[productID] productHandler:^(BOOL success, NSArray<NamiMetaProduct *> * _Nullable products, NSArray<NSString *> * _Nullable invalidProducts, NSError * _Nullable error) {
+        NamiMetaProduct *useProduct = products.firstObject;
+        if (useProduct != nil) {
+            [[NamiStoreKitHelper shared] buyProduct:useProduct fromPaywall:nil responseHandler:^(NSArray<NamiMetaPurchase *> * _Nonnull purchase, NamiPurchaseState purchaseState, NSError * _Nullable error) {
+                if (purchaseState == NamiPurchaseStatePurchased) {
+                    completion(@[[NSNumber numberWithBool:true]]);
+                } else {
+                    completion(@[[NSNumber numberWithBool:success]]);
+                }
+            }];
+        } else {
+            completion(@[[NSNumber numberWithBool:false]]);
+        }
+    }];
+}
+
 @end
 
 @implementation NamiStoreKitHelperBridge
