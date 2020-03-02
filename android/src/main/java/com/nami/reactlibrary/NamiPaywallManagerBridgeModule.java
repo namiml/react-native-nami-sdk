0cb3fa05d888c3ebc547e62f651de6fc5d02470b
Kendall Helmstetter Gelner <Kendall.Gelner@kigisoftware.com>
2020-03-02 15:42:00 -0700

    Added NamiPaywallManagerBridge for Android.

--- android/src/main/java/com/nami/reactlibrary/NamiPaywallManagerBridgeModule.java
+++ android/src/main/java/com/nami/reactlibrary/NamiPaywallManagerBridgeModule.java
@@ -1,5 +1,10 @@
 package com.nami.reactlibrary;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.util.Log;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -10,6 +15,11 @@ import com.namiml.BuildConfig;
 import com.namiml.Nami;
 import com.namiml.NamiConfiguration;
 import com.namiml.NamiLogLevel;
+import com.namiml.NamiPaywallManager;
+
+import org.jetbrains.annotations.NotNull;
+
+
 
 public class NamiPaywallManagerBridgeModule extends ReactContextBaseJavaModule {
     private final ReactApplicationContext reactContext;
@@ -19,8 +29,38 @@ public class NamiPaywallManagerBridgeModule extends ReactContextBaseJavaModule {
         this.reactContext = reactContext;
     }
 
+    @NotNull
     @Override
     public String getName() {
         return "NamiPaywallManagerBridge";
+    }
+
+    @ReactMethod
+    public void raisePaywall()  {
+//        [[NamiPaywallManager shared] raisePaywallFromVC:nil];
+        Activity activity = getActivity(reactContext);
+        NamiPaywallManager.raisePaywall(activity, false);
+        Log.e("ReactNative", "Raising Paywall: ");
+    }
+
+    public Activity getActivity(Context context)
+    {
+        if (context == null)
+        {
+            return null;
+        }
+        else if (context instanceof ContextWrapper)
+        {
+            if (context instanceof Activity)
+            {
+                return (Activity) context;
+            }
+            else
+            {
+                return getActivity(((ContextWrapper) context).getBaseContext());
+            }
+        }
+
+        return null;
     }
 }
