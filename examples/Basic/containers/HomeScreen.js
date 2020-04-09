ab7d5c5c3f963de71d02a3385b3ad2d6234949c7
Kendall Helmstetter Gelner <Kendall.Gelner@kigisoftware.com>
2020-04-09 12:52:53 -0600

    Adjusted configuration for Basic and Analytics apps, added stub analytics emitter for Android.

--- examples/Basic/containers/HomeScreen.js
+++ examples/Basic/containers/HomeScreen.js
@@ -81,16 +81,11 @@ const HomeScreen = (props) => {
     console.log("HavePaywallManager", NativeModules.NamiPaywallManagerBridge)
 
     eventEmitter.addListener('SignInActivate', onSignInActivated);
-
-
-
-
-    //    NativeModules.NamiStoreKitHelperBridge.clearBypassStoreKitPurchases();
     NativeModules.NamiPurchaseManagerBridge.bypassStore(true);
-    //    NativeModules.NamiBridge.configureWithAppID("002e2c49-7f66-4d22-a05c-1dc9f2b7f2af");
 
     var configDict = {
-	'appPlatformID': '002e2c49-7f66-4d22-a05c-1dc9f2b7f2af',
+	'appPlatformID-apple': '002e2c49-7f66-4d22-a05c-1dc9f2b7f2af',
+	'appPlatformID-google': '3d062066-9d3c-430e-935d-855e2c56dd8e',
 	"logLevel": "DEBUG"
     };
     
