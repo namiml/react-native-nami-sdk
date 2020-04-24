8d7e60aa35aa743cbb19ae8f1eb9a6702dc07de7
Kendall Helmstetter Gelner <Kendall.Gelner@kigisoftware.com>
2020-04-23 19:48:01 -0600

    Further Android entitlement updates.

--- android/src/main/java/com/nami/reactlibrary/NamiEntitlementManagerBridgeModule.kt
+++ android/src/main/java/com/nami/reactlibrary/NamiEntitlementManagerBridgeModule.kt
@@ -3,8 +3,10 @@ package com.nami.reactlibrary
 
 import com.facebook.react.bridge.*
 import com.namiml.billing.NamiPurchaseManager
+import com.namiml.entitlement.NamiEntitlement
 import com.namiml.entitlement.NamiEntitlementManager
 import com.namiml.entitlement.NamiEntitlementSetter
+import com.namiml.entitlement.NamiPlatformType
 
 class NamiEntitlementManagerBridgeModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
@@ -56,13 +58,88 @@ class NamiEntitlementManagerBridgeModule(reactContext: ReactApplicationContext) 
             referenceID = entitlements.getString("referenceID") ?: ""
         }
 
-        if (referenceID.length != 0)
-        {
-            var namiSetter = NamiEntitlementSetter(referenceId = "")
+        if (referenceID.length != 0) {
+            var namiSetter = NamiEntitlementSetter(referenceID)
             namiSetter.expires = ///???  String should be date?
-            namiSetter.platform = android
+                    namiSetter.platform = android
             namiSetter.purchasedSKUid = ""
         }
+    }
+
+    fun entitlementDictFromEntitlemment(entitlement: NamiEntitlement): WritableMap? {
+        var resultMap: WritableMap = WritableNativeMap()
+        val referenceID: String = entitlement.referenceId ?: ""
+        resultMap.putString("referenceId", referenceID)
+
+        if (referenceID.isEmpty()) {
+            // Without a reference ID, do not use this object
+            return null
+        }
+
+        val namiID: String = entitlement.namiId ?: ""
+        resultMap.putString("namiID", namiID)
+
+        val description: String = entitlement.desc ?: ""
+        resultMap.putString("description", description)
+
+        val isActive: Boolean = entitlement.isActive() ?: false
+        resultMap.putBoolean("isActive", isActive)
+
+        val platformStr: String = when (entitlement.platform) {
+            NamiPlatformType.OTHER -> "other"
+
+            NamiPlatformType.ANDROID -> "android"
+
+            NamiPlatformType.APPLE -> "apple"
+
+            NamiPlatformType.ROKU -> "roku"
+
+            NamiPlatformType.WEB -> "web"
+
+            NamiPlatformType.UNKNOWN -> "unknown"
+        }
+        resultMap.putString("platformStr", platformStr)
+
+        val activePurchasesArray: WritableArray = WritableNativeArray()
+        val purchases = entitlement.activePurchases
+        purchases?.let {
+            for (purchase in purchases) {
+                val purchaseMap = purchaseToPurchaseDict(purchase)
+                activePurchasesArray.pushMap(purchaseMap)
+            }
+        }
+        resultMap.putArray("activePurchasesArray", activePurchasesArray)
+
+
+        val purchasedSKUsArray: WritableArray = WritableNativeArray()
+        val purchasedSKUs = entitlement.purchasedSKUs
+        for (sku in purchasedSKUs) {
+            val skuMap = skuToSkuDict(sku)
+            purchasedSKUsArray.pushMap(skuMap)
+        }
+        resultMap.putArray("purchasedSKUs", purchasedSKUsArray)
+
+
+        val relatedSKUsArray: WritableArray = WritableNativeArray()
+        val relatedSKUs = entitlement.relatedSKUs
+        for (sku in relatedSKUs) {
+            val skuMap = skuToSkuDict(sku)
+            relatedSKUsArray.pushMap(skuMap)
+        }
+        resultMap.putArray("relatedSKUs", relatedSKUsArray)
+
+        // For react, provide the most recent active purchase and sku from the arrays
+
+        val lastPurchaseSkKU = entitlement.purchasedSKUs.last()
+        lastPurchaseSkKU.let { resultMap.putMap("purchasedSKU", skuToSkuDict(lastPurchaseSkKU)) }
+
+        val lastPurchase = entitlement.activePurchases.last()
+        lastPurchase.let { resultMap.putMap("activePurchase", purchaseToPurchaseDict(lastPurchase)) }
+
+        val namiID: String = entitlement.?: ""
+        resultMap.putString("namiID", namiID)
+
+        return resultMap
     }
 
 }
