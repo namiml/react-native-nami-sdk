16d33a103ba8449631e7105f35cb94fa58249336
Alejandro Cantarero <cantarer@gmail.com>
2020-05-21 22:06:06 -0700

    load core content from correct location

--- examples/Basic/containers/AboutScreen.js
+++ examples/Basic/containers/AboutScreen.js
@@ -17,11 +17,11 @@ const AboutScreen = (props) => {
 
   useEffect(() => {
     console.log('ExampleApp: Entering About Screen.');
-    NativeModules.NamiBridge.enterCoreContentWithLabel("AboutScreen");
+    NativeModules.NamiMLManagerBridge.enterCoreContentWithLabel("AboutScreen");
 
     return () => {
       console.log('ExampleApp: Exiting About Screen.');
-      NativeModules.NamiBridge.exitCoreContentWithLabel("AboutScreen");
+      NativeModules.NamiMLManagerBridge.exitCoreContentWithLabel("AboutScreen");
     };
   }, [props.navigation]);
 
