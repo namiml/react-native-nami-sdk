d4f4ae6278de5278725e0115a47c02be4bf39512
daiky00 <project.01@hotmail.com>
2020-11-24 00:39:55 -0600

    added unsubscribe callback functions

--- examples/BasicLinkedPaywall/App.js
+++ examples/BasicLinkedPaywall/App.js
@@ -46,8 +46,15 @@ const App = () => {
     );
     console.log(NativeModules.NamiMLManagerBridge, 'NamiMLManagerBridge');
 
-    eventEmitter.addListener('PurchasesChanged', onSessionConnect);
-    eventEmitter.addListener('AppPaywallActivate', onPaywallShouldRaise);
+    let PurchasedSubscription = eventEmitter.addListener(
+      'PurchasesChanged',
+      onSessionConnect,
+    );
+
+    let PaywallShouldRaiseSubscription = eventEmitter.addListener(
+      'AppPaywallActivate',
+      onPaywallShouldRaise,
+    );
 
     var configDict = {
       'appPlatformID-google': 'a95cef52-35e0-4794-8755-577492c2d5d1',
@@ -62,6 +69,10 @@ const App = () => {
     NativeModules.NamiPaywallManagerBridge.canRaisePaywall((result) => {
       console.log('ExampleApp: Nami canRaisePaywall ', result);
     });
+    return () => {
+      PurchasedSubscription.remove();
+      PaywallShouldRaiseSubscription.remove();
+    };
   }, []);
 
   return <AppNavigation />;
