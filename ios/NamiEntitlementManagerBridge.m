ccdaf6cce721b4a3b76d635c43015a78eafc2e07
Andrii Novoselskyi <andrii.novoselskyi@elbax.com>
2023-03-24 19:30:58 +0200

    migration - updated NamiEntitlementManagerBridge

--- ios/NamiEntitlementManagerBridge.m
+++ ios/NamiEntitlementManagerBridge.m
@@ -64,52 +64,6 @@ RCT_EXPORT_METHOD(activeEntitlements:(RCTResponseSenderBlock)completion)
     completion(@[entitlementDicts]);
 }
 
-//    Didn't found new API
-//RCT_EXPORT_METHOD(setEntitlements:(NSArray *)entitlementSetterDicts)
-//{
-//
-//    NSMutableArray<NamiEntitlementSetter *> *entitlementSetters = [NSMutableArray array];
-//
-//    for (NSDictionary *entitlementSetterDict in entitlementSetterDicts) {
-//        NSString *referenceID = entitlementSetterDict[@"referenceID"];
-//        NSLog(@"Entitlement to set, referenceID is %@, whole entitlement %@", referenceID, entitlementSetterDict);
-//        if (referenceID != NULL && [referenceID length] > 0) {
-//            NSString *purchasedSKUid = entitlementSetterDict[@"purchasedSKUid"];
-//            NSString *expiresStr = entitlementSetterDict[@"expires"];
-//            // TODO: figure out reverse parsing of date string passed in
-//            NSDate* expires = NULL;
-//
-//            NSString *platormStr = entitlementSetterDict[@"platform"];
-//
-//            NamiPlatformType platform = NamiPlatformTypeOther;
-//            if ([platormStr isEqualToString:@"android"]) {
-//                platform = NamiPlatformTypeAndroid;
-//            } else if ([platormStr isEqualToString:@"apple"]) {
-//                platform = NamiPlatformTypeApple;
-//            } else if ([platormStr isEqualToString:@"roku"]) {
-//                platform = NamiPlatformTypeRoku;
-//            } else if ([platormStr isEqualToString:@"web"]) {
-//                platform = NamiPlatformTypeWeb;
-//            }
-//
-//            NamiEntitlementSetter *setter = [NamiEntitlementSetter alloc];
-//            setter = [setter initWithId:referenceID platform:platform purchasedSKUid:purchasedSKUid expires:expires];
-//
-//            [entitlementSetters addObject:setter];
-//        } else {
-//            NSLog(@"Warning, entitlement to set had empty referenceID, whole entitlement is \n %@", entitlementSetterDict);
-//        }
-//    }
-//
-//    [NamiEntitlementManager setEntitlements:entitlementSetters];
-//}
-
-//    Didn't found new API
-//RCT_EXPORT_METHOD(clearAllEntitlements) {
-//    [NamiEntitlementManager clearAllEntitlements];
-//}
-
-
 @end
 
 
