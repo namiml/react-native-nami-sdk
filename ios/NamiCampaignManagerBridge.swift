368ce64f0dbd7e22d98e11456e13a6b20ecda36d
Ivan Madera <gmimadera@gmail.com>
2023-04-26 13:35:56 +0300

    change launch

--- ios/NamiCampaignManagerBridge.swift
+++ ios/NamiCampaignManagerBridge.swift
@@ -27,11 +27,44 @@ class RNNamiCampaignManager: RCTEventEmitter {
         return NSDictionary(dictionary: dictionary.compactMapValues { $0 })
     }
     
-    @objc(launch:completion:)
-    func launch(label: String?, callback: @escaping RCTResponseSenderBlock) -> Void {
+    @objc(launch:completion:paywallCompletion:)
+    func launch(label: String?, callback: @escaping RCTResponseSenderBlock, paywallCallback: @escaping RCTResponseSenderBlock) -> Void {
         NamiCampaignManager.launch(label: label, launchHandler: { success, error in
             callback([success, error?._code as Any])
-         })
+        }, paywallActionHandler: { action, sku, purchaseError, purchases -> () in
+            let actionString: String
+            switch action {
+            case .close_paywall:
+                actionString = "NAMI_CLOSE_PAYWALL"
+            case .restore_purchases:
+                actionString = "NAMI_RESTORE_PURCHASES"
+            case .sign_in:
+                actionString = "NAMI_SIGN_IN"
+            case .buy_sku:
+                actionString = "NAMI_BUY_SKU"
+            case .select_sku:
+                actionString = "NAMI_SELECT_SKU"
+            case .purchase_selected_sku:
+                actionString = "NAMI_PURCHASE_SELECTED_SKU"
+            case .purchase_success:
+                actionString = "NAMI_PURCHASE_SUCCESS"
+            case .purchase_deferred:
+                actionString = "NAMI_PURCHASE_DEFERRED"
+            case .purchase_failed:
+                actionString = "NAMI_PURCHASE_FAILED"
+            case .purchase_cancelled:
+                actionString = "NAMI_PURCHASE_CANCELLED"
+            case .purchase_unknown:
+                actionString = "NAMI_PURCHASE_UNKNOWN"
+            @unknown default:
+                actionString = "NAMI_PURCHASE_UNKNOWN"
+            }
+            let skuId = sku?.skuId
+            let errorSting = purchaseError?.localizedDescription
+            let dictionaries = purchases.map { purchase in NamiBridgeUtil.purchase(toPurchaseDict: purchase) }
+            callback([actionString, skuId as Any, errorSting as Any, dictionaries])
+        })
+        
     }
 
     @objc(allCampaigns:rejecter:)
