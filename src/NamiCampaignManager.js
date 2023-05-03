808faf1949c57b565c98634329ac4ebae1c9e308
Ivan Madera <gmimadera@gmail.com>
2023-05-03 10:48:26 +0300

    launch fix

--- src/NamiCampaignManager.js
+++ src/NamiCampaignManager.js
@@ -3,18 +3,21 @@ import { NativeModules, NativeEventEmitter } from "react-native";
 export const { RNNamiCampaignManager } = NativeModules;
 
 export const NamiCampaignManager = {
+  launchSubscription: undefined,
   emitter: new NativeEventEmitter(RNNamiCampaignManager),
   ...RNNamiCampaignManager,
   launch(label, actionCallback, resultCallback) {
-    var subscription;
-    subscription = this.emitter.addListener("ResultCampaign", (body) => {
-      var action = body.action;
-      var skuId = body.skuId;
-      var purchaseError = body.purchaseError;
-      var purchases = body.purchases;
-      resultCallback(action, skuId, purchaseError, purchases);
-      // subscription.remove();
-    });
+    this.launchSubscription?.remove();
+    this.launchSubscription = this.emitter.addListener(
+      "ResultCampaign",
+      (body) => {
+        var action = body.action;
+        var skuId = body.skuId;
+        var purchaseError = body.purchaseError;
+        var purchases = body.purchases;
+        resultCallback(action, skuId, purchaseError, purchases);
+      }
+    );
     RNNamiCampaignManager.launch(
       label ?? null,
       actionCallback ?? (() => {}),
