dcd708c87982072bcaa7937c3dec404a38cec408
dannami <dan.burcaw@namiml.com>
2023-05-27 17:28:31 -0600

    update CI and test app for tvOS

--- examples/Basic/App.tsx
+++ examples/Basic/App.tsx
@@ -5,6 +5,7 @@ import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NamiCustomerManager} from 'react-native-nami-sdk';
 import {NamiPaywallManager} from 'react-native-nami-sdk';
+
 import CampaignScreen from './containers/CampaignScreen';
 import ProfileScreen from './containers/ProfileScreen';
 import EntitlementsScreen from './containers/EntitlementsScreen';
@@ -32,56 +33,51 @@ const Tab = createBottomTabNavigator<ViewerTabNavigatorParams>();
 
 const App = () => {
   useEffect(() => {
-    console.log('app');
+    NamiPaywallManager.registerBuySkuHandler((sku) => {
+      console.log(
+        'buy sku handler - need to start purchase flow for sku:',
+        sku.skuId,
+      );
 
-    const subscriptionRemover = NamiPaywallManager.registerBuySkuHandler(
-      (sku) => {
-        console.log('sku', sku);
-        console.log(
-          'buy sku handler - need to start purchase flow for sku:',
-          sku.skuId,
-        );
-        NamiPaywallManager.dismiss(true);
-        if (Platform.OS === 'ios' || Platform.isTVOS) {
-          NamiPaywallManager.buySkuCompleteApple({
+      NamiPaywallManager.dismiss(true);
+
+      if (Platform.OS === 'ios' || Platform.isTVOS) {
+        NamiPaywallManager.buySkuCompleteApple({
+          product: sku,
+          transactionID: '12345',
+          originalTransactionID: '12345',
+          originalPurchaseDate: 1684823428,
+          purchaseDate: 1684823428,
+          price: '120',
+          currencyCode: 'USD',
+          locale: 'US',
+        });
+      } else if (Platform.OS === 'android') {
+        if (Platform.constants.Manufacturer === 'Amazon') {
+          NamiPaywallManager.buySkuCompleteAmazon({
             product: sku,
-            transactionID: '12345',
-            originalTransactionID: '12345',
-            originalPurchaseDate: 1684823428,
             purchaseDate: 1684823428,
-            price: '120',
-            currencyCode: 'USD',
-            locale: 'US',
+            purchaseSource: 'CAMPAIGN',
+            receiptId: '12345',
+            localizedPrice: '120',
+            userId: '12345',
+            marketplace: '12345',
           });
-        } else if (Platform.OS === 'android') {
-          if (Platform.constants.Manufacturer === 'Amazon') {
-            NamiPaywallManager.buySkuCompleteAmazon({
-              product: sku,
-              purchaseDate: 1684823428,
-              purchaseSource: 'CAMPAIGN',
-              receiptId: '12345',
-              localizedPrice: '120',
-              userId: '12345',
-              marketplace: '12345',
-            });
-          } else {
-            NamiPaywallManager.buySkuCompleteGooglePlay({
-              product: sku,
-              purchaseDate: 1684823428,
-              purchaseSource: 'CAMPAIGN',
-              purchaseToken:
-                'jolbnkpmojnpnjecgmphbmkc.AO-J1OznE4AIzyUvKFe1RSVkxw4KEtv0WfyL_tkzozOqnlSvIPsyQJBphCN80gwIMaex4EMII95rFCZhMCbVPZDc-y_VVhQU5Ddua1dLn8zV7ms_tdwoDmE',
-              orderId: 'GPA.3317-0284-9993-42221',
-            });
-          }
+        } else {
+          NamiPaywallManager.buySkuCompleteGooglePlay({
+            product: sku,
+            purchaseDate: 1684823428,
+            purchaseSource: 'CAMPAIGN',
+            purchaseToken:
+              'jolbnkpmojnpnjecgmphbmkc.AO-J1OznE4AIzyUvKFe1RSVkxw4KEtv0WfyL_tkzozOqnlSvIPsyQJBphCN80gwIMaex4EMII95rFCZhMCbVPZDc-y_VVhQU5Ddua1dLn8zV7ms_tdwoDmE',
+            orderId: 'GPA.3317-0284-9993-42221',
+          });
         }
-      },
-    );
+      }
+    });
 
     NamiCustomerManager.setCustomerDataPlatformId('2135');
-    return () => {
-      subscriptionRemover();
-    };
+    return () => {};
   }, []);
 
   return (
--- examples/TestNamiTV/metro.config.js
+++ examples/TestNamiTV/metro.config.js
@@ -1,24 +1,16 @@
-const path = require('path');
-
-const thirdPartyPath = path.resolve(__dirname + '/../TestNamiTV/'); // Path of your local module
-
-const thirdParty = {
-  'react-native-nami-sdk': thirdPartyPath,
-};
-const watchFolders = [thirdPartyPath];
+/* Metro configuration for React Native
+* https://github.com/facebook/react-native
+*
+* @format
+*/
 
 module.exports = {
-  // existing dependencies
-  resolver: {
-    thirdParty,
-  },
-  watchFolders,
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
+ transformer: {
+   getTransformOptions: async () => ({
+     transform: {
+       experimentalImportSupport: false,
+       inlineRequires: true,
+     },
+   }),
+ },
 };
