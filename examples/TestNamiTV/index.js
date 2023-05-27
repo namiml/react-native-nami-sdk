054b2cb195f8f9958659f645caef17ad9f6336d3
dannami <dan.burcaw@namiml.com>
2023-05-26 23:27:05 -0600

    add tvos prod target and other misc

--- examples/TestNamiTV/index.js
+++ examples/TestNamiTV/index.js
@@ -2,20 +2,35 @@
  * @format
  */
 import {AppRegistry, Platform} from 'react-native';
-import {Nami, NamiLanguageCodes} from 'react-native-nami-sdk';
+import {NativeModules} from 'react-native';
+import {Nami} from 'react-native-nami-sdk';
 import App from './App';
 
-let appPlatformIDAndroid = 'b7232eba-ff1d-4b7f-b8d0-55593b66c1d5';
-
+let flavor = NativeModules.RNConfig.FLAVOR;
+let appPlatformIDApple = '4a2f6dbf-e684-4d65-a4df-0488771c577d';
+let appPlatformIDAndroid = '9cdda53d-fcb1-4d5b-b8b7-575437b6fe34';
 if (Platform.OS === 'android' && Platform.constants.Manufacturer === 'Amazon') {
-  appPlatformIDAndroid = 'd5256425-e827-4ae4-970e-66d027dc1108';
+  appPlatformIDAndroid = '76a40e56-f4df-4bcd-a1d0-615408caaec1';
+}
+let namiCommands = null;
+
+if (flavor === 'staging') {
+  appPlatformIDApple = '4a2f6dbf-e684-4d65-a4df-0488771c577d';
+  appPlatformIDAndroid = 'b7232eba-ff1d-4b7f-b8d0-55593b66c1d5';
+  if (
+    Platform.OS === 'android' &&
+    Platform.constants.Manufacturer === 'Amazon'
+  ) {
+    appPlatformIDAndroid = 'd5256425-e827-4ae4-970e-66d027dc1108';
+  }
+  namiCommands = ['useStagingAPI'];
 }
 
 let configDict = {
-  'appPlatformID-apple': '4a2f6dbf-e684-4d65-a4df-0488771c577d',
+  'appPlatformID-apple': appPlatformIDApple,
   'appPlatformID-android': appPlatformIDAndroid,
   logLevel: 'DEBUG',
-  namiCommands: ['useStagingAPI'],
+  namiCommands: namiCommands ?? [],
 };
 
 Nami.configure(configDict);
