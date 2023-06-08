5a98423cbfa29d6d4441f7a5343d7761ce3dd371
dannami <dan.burcaw@namiml.com>
2023-06-08 12:14:47 -0600

    initial config support

--- examples/TestNamiTV/index.js
+++ examples/TestNamiTV/index.js
@@ -30,23 +30,22 @@ let configDict = {}
 if (flavor === 'staging') {
   configDict = {
     'appPlatformID-apple': '4a2f6dbf-e684-4d65-a4df-0488771c577d',
-    'appPlatformID-android': appPlatformIDAndroid,
-    'appPlatformID-android': amazonDevice == true ?  'd5256425-e827-4ae4-970e-66d027dc1108' ?? 'b7232eba-ff1d-4b7f-b8d0-55593b66c1d5',
+    'appPlatformID-android': amazonDevice == true ?  'd5256425-e827-4ae4-970e-66d027dc1108' : 'b7232eba-ff1d-4b7f-b8d0-55593b66c1d5',
     logLevel: 'DEBUG',
     namiCommands: ['useStagingAPI'],
   };
 
-  if (initialConfig != '') {
+  if (namiInitialConfigStg != '') {
     configDict['initialConfig'] = namiInitialConfigStg;
   };
 } else {
   configDict = {
     'appPlatformID-apple': 'e1e51d49-5bda-41b2-9367-8408bb374b07',
-    'appPlatformID-android': amazonDevice == true ?  '76a40e56-f4df-4bcd-a1d0-615408caaec1' ?? '9cdda53d-fcb1-4d5b-b8b7-575437b6fe34',
+    'appPlatformID-android': amazonDevice == true ?  '76a40e56-f4df-4bcd-a1d0-615408caaec1' : '9cdda53d-fcb1-4d5b-b8b7-575437b6fe34',
     logLevel: 'WARN',
   };
 
-  if (initialConfig != '') {
+  if (namiInitialConfigProd != '') {
     configDict['initialConfig'] = namiInitialConfigProd;
   };
 
