0a5e1f58ad5e79ec8f768a1bec51909fb1bd7364
Dan Burcaw <92536821+namidan@users.noreply.github.com>
2023-06-16 13:19:47 -0600

    3.0.14 (#191)
    * add initial config
    * 3.0.14

--- android/src/main/java/com/nami/reactlibrary/NamiBridgeModule.kt
+++ android/src/main/java/com/nami/reactlibrary/NamiBridgeModule.kt
@@ -115,7 +115,7 @@ class NamiBridgeModule(reactContext: ReactApplicationContext) :
             } else {
                 Arguments.createArray()
             }
-        val settingsList = mutableListOf("extendedClientInfo:react-native:3.0.13")
+        val settingsList = mutableListOf("extendedClientInfo:react-native:3.0.14")
         namiCommandsReact?.toArrayList()?.filterIsInstance<String>()?.let { commandsFromReact ->
             settingsList.addAll(commandsFromReact)
         }
