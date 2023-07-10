2c9084141db6b5a32bfef151196762a87c6fbcff
bataevvlad <bataevvlad@gmail.com>
2023-07-10 21:22:58 +0200

    fix(detox): Push change for detox test #7.

--- examples/Basic/e2e/jest.config.js
+++ examples/Basic/e2e/jest.config.js
@@ -1,9 +1,9 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
+  maxWorkers: 1,
   rootDir: '..',
   testMatch: ['<rootDir>/e2e/**/*.test.js'],
   testTimeout: 120000,
-  maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
   reporters: ['detox/runners/jest/reporter'],
