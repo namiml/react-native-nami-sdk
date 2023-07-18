d4cc1c36cd65800ebdbad7f5975a0d8fae32c3f9
bataevvlad <bataevvlad@gmail.com>
2023-07-18 12:13:43 +0200

    fix(ios-tests): Fix yml Android #63

--- examples/Basic/.detoxrc.js
+++ examples/Basic/.detoxrc.js
@@ -24,14 +24,16 @@ module.exports = {
     },
     'android.debug': {
       type: 'android.apk',
-      binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+      binaryPath: 'android/app/build/outputs/apk/staging/debug/app-staging-debug.apk',
+      testBinaryPath: 'android/app/build/outputs/apk/staging/debug/app-staging-debug.apk',
       build:
         'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
       reversePorts: [8081],
     },
     'android.release': {
       type: 'android.apk',
-      binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
+      binaryPath: 'android/app/build/outputs/apk/production/release/app-release.apk',
+      testBinaryPath: 'android/app/build/outputs/apk/production/release/app-release.apk',
       build:
         'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release',
     },
