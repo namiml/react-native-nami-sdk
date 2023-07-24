e1e993064b59c4143cdb3f361e1e9ded003a4d4c
bataevvlad <bataevvlad@gmail.com>
2023-07-24 09:34:48 +0200

    fix(ci-tests): Android updates for CI #2.

--- examples/Basic/.detoxrc.js
+++ examples/Basic/.detoxrc.js
@@ -54,7 +54,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3a_API_33',
+        avdName: 'Pixel_API_29_AOSP',
       },
     },
   },
