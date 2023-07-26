da5dec39bf2c50cd5377dc0fb04252c88c2dda19
bataevvlad <bataevvlad@gmail.com>
2023-07-26 16:17:46 +0200

    fix(ci-tests): Last call android #1.

--- examples/Basic/.detoxrc.js
+++ examples/Basic/.detoxrc.js
@@ -54,7 +54,6 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        // avdName: 'Pixel_4_API_30',
         avdName: 'Pixel_3a_API_30_AOSP',
       },
     },
