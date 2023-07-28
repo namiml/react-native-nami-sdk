9b4b0c93851e864341d9661f2b731b7f80a532d2
bataevvlad <bataevvlad@gmail.com>
2023-07-28 15:56:37 +0200

    fix(merge): Detox tests fixes #5.

--- examples/Basic/e2e/main.test.js
+++ examples/Basic/e2e/main.test.js
@@ -1,8 +1,7 @@
 import { device, element, by, expect, waitFor, log } from 'detox';
 
 const data = {
-  campaign: 'puffin',
-  // campaign: 'trident',
+  campaign: 'penguin',
 };
 
 describe('Configure Test', () => {
