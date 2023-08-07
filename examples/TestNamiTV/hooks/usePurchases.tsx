24aa878e96be792194f582f967f47de0302b18e4
Ivan Vezhnavets <whitestranger7@gmail.com>
2023-08-07 12:30:54 +0200

    fix: typescipt, linting and all coresponding fixes for nami tv example

--- examples/TestNamiTV/hooks/usePurchases.tsx
+++ examples/TestNamiTV/hooks/usePurchases.tsx
@@ -1,22 +1,38 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useMemo } from 'react';
+
+interface IPurchasesContext {
+  children: React.ReactNode;
+}
+
+interface PurchasesContextType {
+  purchases: string[];
+  setPurchases: (purchases: string[]) => void;
+}
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-const PurchasesContext = createContext([[], () => {}]);
+const PurchasesContext = createContext<PurchasesContextType | null>(null);
 
-export const PurchasesContextProvider = ({ children }) => {
-  const [purchases, setPurchases] = useState<[] as any[]>([]);
+export const PurchasesContextProvider = ({ children }: IPurchasesContext) => {
+  const [purchases, setPurchases] = useState<string[]>([]);
+
+  const value = useMemo(() => ({
+    purchases,
+    setPurchases,
+  }), [purchases, setPurchases])
+
   return (
-    <PurchasesContext.Provider value={[purchases, setPurchases]}>
+    <PurchasesContext.Provider value={value}>
       {children}
     </PurchasesContext.Provider>
   );
 };
 
 export const usePurchasesContext = () => {
-  const [purchases, setPurchases] = useContext(PurchasesContext);
+  const purchasesContext = useContext(PurchasesContext);
 
-  return {
-    purchases,
-    setPurchases,
-  };
+  if (!purchasesContext) {
+    throw new Error('calling usePurchasesContext out of PurchasesContext');
+  }
+
+  return purchasesContext;
 };
