94b07c2157c08f7de2487569b8713cb4daab51e4
Dan Burcaw <92536821+namidan@users.noreply.github.com>
2024-01-09 19:35:35 -0700

    3.1.27 (#292)

--- android/src/main/java/com/nami/reactlibrary/NamiBridgeModule.kt
+++ android/src/main/java/com/nami/reactlibrary/NamiBridgeModule.kt
@@ -106,7 +106,7 @@ class NamiBridgeModule(reactContext: ReactApplicationContext) :
             } else {
                 Arguments.createArray()
             }
-        val settingsList = mutableListOf("extendedClientInfo:react-native:3.1.26")
+        val settingsList = mutableListOf("extendedClientInfo:react-native:3.1.27")
         namiCommandsReact?.toArrayList()?.filterIsInstance<String>()?.let { commandsFromReact ->
             settingsList.addAll(commandsFromReact)
         }
