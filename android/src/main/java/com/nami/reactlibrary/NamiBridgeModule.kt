a49917c6eec6550a9ee87f48e30118aab355213d
dannami <dan.burcaw@namiml.com>
2024-06-20 12:30:17 -0600

    3.1.36

--- android/src/main/java/com/nami/reactlibrary/NamiBridgeModule.kt
+++ android/src/main/java/com/nami/reactlibrary/NamiBridgeModule.kt
@@ -106,7 +106,7 @@ class NamiBridgeModule(reactContext: ReactApplicationContext) :
             } else {
                 Arguments.createArray()
             }
-        val settingsList = mutableListOf("extendedClientInfo:react-native:3.1.35")
+        val settingsList = mutableListOf("extendedClientInfo:react-native:3.1.36")
         namiCommandsReact?.toArrayList()?.filterIsInstance<String>()?.let { commandsFromReact ->
             settingsList.addAll(commandsFromReact)
         }
