f6f9104eb89479df621becdf1bb0f433d7417757
dannami <dan.burcaw@namiml.com>
2024-07-23 14:10:09 -0600

    eslint

--- examples/TestNamiTV/metro.config.js
+++ examples/TestNamiTV/metro.config.js
@@ -1,4 +1,5 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
