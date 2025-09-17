85b86c3a9a92b772f183593f2d899164c9115fc9
Dan Burcaw <92536821+namidan@users.noreply.github.com>
2025-09-16 19:30:52 -0600

    3.3.3-1 (#404)
    * 3.3.3-1
    
    * 3.3.3-1
    
    * 3.3.3-1
    
    * prefer online
    
    * remove peerDependenciesMeta

--- examples/TestNamiTV/scripts/print-latest-sdk-pack-name.mjs
+++ examples/TestNamiTV/scripts/print-latest-sdk-pack-name.mjs
@@ -1,4 +1,13 @@
-import pkg from '../../../package.json' assert {type: 'json'};
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const packageJsonPath = join(__dirname, '../../../package.json');
+const pkg = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
 
 const name = pkg.name.replace('@', '').replace('/', '-');
 const version = pkg.version;
